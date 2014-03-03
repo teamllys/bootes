fa26aa3cdfb3fae6737dbb05e3fc71c12f670e43
8do8do <8do8do@gmail.com>
2014-03-03 14:33:59 +0900

    add LoginViewController

---
+++ CoffretShow/controller/BootesLoginViewController.m
@@ -0,0 +1,38 @@
+//
+//  BootesLoginViewController.m
+//  CoffretShow
+//
+//  Created by chyoon on 3/3/14.
+//  Copyright (c) 2014 teamllys. All rights reserved.
+//
+
+#import "BootesLoginViewController.h"
+
+@interface BootesLoginViewController ()
+
+@end
+
+@implementation BootesLoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
